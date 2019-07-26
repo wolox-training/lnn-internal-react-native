c55aad4aa5606ee9c32f12b1bf03b7e60786234d
Lucas Nestrojil <lucasnestrojil@gmail.com>
2019-07-26 10:08:38 -0300

    deleted bad file .env

--- app/screens/Library/layout.js
+++ app/screens/Library/layout.js
@@ -8,11 +8,11 @@ import img from './assets/img_book1.png';
 function Library() {
   return (
     <View style={styles.container}>
-    <Book
-      title='A Little Bird Told Me'
-      author='Timothy Cross'
-      img={null}
-    />
+      <Book
+        title='A Little Bird Told Me'
+        author='Timothy Cross'
+        img={img}
+      />
     </View>
   );
 }
