6c925c31ed3ed67c889600bfeffa35608639b962
Lucas Nestrojil <lucasnestrojil@gmail.com>
2019-07-29 16:49:08 -0300

    Deleted comment

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import AppNavigator from './navigation/AppNavigation';
-// import LibraryContainer from './app/screens/Library/index';
 
 const App = () => {
   return <AppNavigator />;
