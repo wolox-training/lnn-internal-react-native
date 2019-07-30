3ddce2048d12d0ee05f57dee54a4b51310195338
Lucas Nestrojil <lucasnestrojil@gmail.com>
2019-07-30 16:16:10 -0300

    Added provisory hardcode by complain card layout

--- app/screens/BookDetail/components/Comment/index.js
+++ app/screens/BookDetail/components/Comment/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Text, View, Image } from 'react-native';
 
-import imagen from '../../../../../assets/img_user1.png';
+import { commentDataPropType } from '../../../../../propTypes';
+// provisory
+import imgTest1 from '../../../../../assets/img_user1.png';
+import imgTest2 from '../../../../../assets/img_user2.png';
 
 import { styles } from './styles';
 
-function Comment({ data: { user_name: userName, comment, imageUrl } }) {
+// provisory
+const imgTest = [imgTest1, imgTest2];
+
+function Comment({ data: { id, user_name: userName, comment, image_url: imageUrl } }) {
+  // provisory
+  const index = id - 1;
   return (
     <View style={styles.container}>
-      <Image style={styles.commentImage} source={imageUrl ? { uri: imageUrl } : imagen} />
+      <Image style={styles.commentImage} source={imageUrl ? { uri: imageUrl } : imgTest[index]} />
       <View style={styles.textContainer}>
         <Text style={styles.commentUser}>{userName}</Text>
         <Text style={styles.commentText}>{comment}</Text>
@@ -19,12 +27,7 @@ function Comment({ data: { user_name: userName, comment, imageUrl } }) {
 }
 
 Comment.propTypes = {
-  data: PropTypes.shape({
-    comment: PropTypes.string.isRequired,
-    // eslint-disable-next-line camelcase
-    user_name: PropTypes.string.isRequired,
-    imageUrl: PropTypes.string
-  })
+  data: PropTypes.shape(commentDataPropType).isRequired
 };
 
 export default Comment;
