7f346d2651a1fd580f24f077e42f0ca1c67f3909
Lucas Nestrojil <lucasnestrojil@gmail.com>
2019-09-20 15:25:30 -0300

    Resolved conflicts

--- redux/Login/actions.js
+++ redux/Login/actions.js
@@ -1,10 +1,7 @@
 import { NavigationActions } from 'react-navigation';
 import LoginService from '@services/LoginService';
-<<<<<<< HEAD
 import LocalStorageService from '@services/LocalStorageService';
-=======
 import { ROUTES } from '@config/screens';
->>>>>>> login-hocs
 
 export const actionTypes = {
   LOGIN: 'LOGIN',
@@ -16,20 +13,16 @@ const login = (user, pass) => dispatch => {
   dispatch({ type: actionTypes.LOGIN });
   LoginService.login(user, pass).then(res => {
     if (res.ok) {
-      dispatch({ type: actionTypes.LOGIN_SUCCESS, payload: res });
+      const sessionData = {
+        accessToken: res.headers['access-token'],
+        client: res.headers.client,
+        uid: res.headers.uid
+      };
+      dispatch({ type: actionTypes.LOGIN_SUCCESS, payload: sessionData });
+      LocalStorageService.setStoreData('access-token', sessionData.accessToken);
       return dispatch(NavigationActions.navigate({ routeName: ROUTES.LIBRARY }));
     }
-<<<<<<< HEAD
-    const sessionData = {
-      accessToken: res.headers['access-token'],
-      client: res.headers.client,
-      uid: res.headers.uid
-    };
-    dispatch({ type: actionTypes.LOGIN_SUCCESS, sessionData });
-    return LocalStorageService.setStoreData('access-token', sessionData.accessToken);
-=======
     return dispatch({ type: actionTypes.LOGIN_FAILURE, payload: res });
->>>>>>> login-hocs
   });
 };
 
