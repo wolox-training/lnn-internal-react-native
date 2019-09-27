e1aea9d5099c9eb5d302172b85b9e9e17333dffa
Lucas Nestrojil <lucasnestrojil@gmail.com>
2019-09-27 15:00:40 -0300

    Extract icon size into constants

--- app/components/navigationComponents/LogoutButton/styles.js
+++ app/components/navigationComponents/LogoutButton/styles.js
@@ -1,9 +1,10 @@
 import { StyleSheet } from 'react-native';
+import { HEADER_ICON_HEIGHT, HEADER_ICON_WIDTH } from '@constants/icons';
 
 export const styles = StyleSheet.create({
   image: {
-    height: 20,
-    width: 20,
+    height: HEADER_ICON_HEIGHT,
+    width: HEADER_ICON_WIDTH,
     marginLeft: 20
   }
 });
